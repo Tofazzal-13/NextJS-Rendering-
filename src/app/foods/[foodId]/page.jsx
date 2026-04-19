9bce4f0c68de7dd089b605103c25a706d7cc26ff
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-04-19 10:10:49 +0600

    Use Loading, Handling Error and Route-Specific Data Load

--- src/app/foods/[foodId]/page.jsx
+++ src/app/foods/[foodId]/page.jsx
@@ -1,12 +1,27 @@
+import Image from 'next/image';
 import React from 'react';
 
 const FoodDetailpage = async ({params}) => {
     const {foodId} = await params;
-     
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/foods/${foodId}`)
+    const data = await res.json();
+    const food = data.data;
+    const {id, dish_name, image_link} = food
 
     return (
         <div>
             <h2>Show details of {foodId}</h2>
+            <h3 className='text-3xll'>{dish_name}</h3>
+            <Image 
+            src={image_link}
+            alt={dish_name}
+            width={300}
+            height={300}
+
+            >
+
+            </Image>
+            
         </div>
     );
 };
---
+++ src/app/foods/loading.jsx
@@ -0,0 +1,11 @@
+
+
+const loading = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <span className="loading loading-spinner text-success"></span>
+        </div>
+    );
+};
+
+export default loading;
